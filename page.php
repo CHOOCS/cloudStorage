0cbbfbba82458a337b26efbf04d1626e53d72238
CHOOCS <35173886+CHOOCS@users.noreply.github.com>
2022-12-19 15:30:31 +0800

    ihack

---
+++ page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+  <title>Cloud Storage</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</head>
+
+  <?php
+  $allowed_files = array('index.html', 'upload.html');
+  $file = $_GET["show"];
+
+  if (in_array($file, $allowed_files)) {
+    include($file);
+  } else {
+    echo "Error: The file you requested is not allowed.";
+  }
+?>
+
+  </body>
+  </html>
